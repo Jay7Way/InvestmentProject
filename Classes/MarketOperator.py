e3b6bbe4385eb5e53adbbf8bf6bf3e832aec8405
Jamie Kane <jamkane@deloitte.nl>
2019-09-24 14:42:00 +0200

    update to marketOperator

--- Classes/MarketOperator.py
+++ Classes/MarketOperator.py
@@ -1,19 +1,20 @@
 class MarketOperator:
     value = 0
 
+    def __init__(self, initialValue):
+        self.value = initialValue
+
     def profit(self, margin):
         self.value += margin
         print("New profit for the operator is: " + str(margin) + "\n"
          "Total earnings by the operator are:  " + str(self.value), "\n \n")
 
 
-operator = MarketOperator()
+operator = MarketOperator(1000)
 
 print(operator.value)
 
 
 operator.profit(100)
-operator.profit(1000)
-operator.profit(10000)
 
 
