438254d5b6cf34896170ced9c822ea58bc9031fe
PvDusseldorp <pvandusseldorp@deloitte.nl>
2019-09-24 16:33:30 +0200

    added

--- Classes/UI.py
+++ Classes/UI.py
@@ -1,17 +1,25 @@
-from Classes.Account import Account
+
+
+class Account(object):
+
+    def __init__(self, name, id, balance, portfolio, orders):
+        self.name = name
+        self.id = id
+        self.balance = balance
+        self.portfolio = portfolio
+        self.orders = orders
+
 
 accounts = []
 # Add account
-class AddAccount(object):
-    answer_1 = input("Do you want to add an account? (Y/N)").upper()
+def AddAccount(name, id, balance, portfolio, orders):
+        accounts.append(name, id, balance, portfolio, orders)
 
-    if answer_1 != "Y":
-        exit
-    else:
-        name = input("Please insert name: ")
-        id = input("Please insert ID: ")
-        balance = input("Please insert balance: ")
-        portfolio = []
-        orders = []
-        accounts.append(Account(name, id, balance, portfolio, orders))
+answer_1 = input("Do you want to add an account? (Y/N)").upper()
 
+if answer_1 != "Y":
+    ""
+else:
+    AddAccount(name = input("Please insert name: "), id = input("Please insert ID: "), balance = input("Please insert balance: "), portfolio = [], orders = [])
+
+print(accounts)
