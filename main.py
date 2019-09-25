08cc6cb0edfbdc3980e745358457d34c36d54dd4
Hackerman <b.chroneer@gmail.com>
2019-09-25 16:06:19 +0200

    updates to main loop

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Functions.AccountsCreator import accountsCreator
 from Functions.MainFunctions import getAccount
 
 traded_tickers=["AAPL", "GOOGL", "INTC", "MFST"]
-MktInput = {"AAPL" : int(dataParser("AAPL", "latest"))}
+AAPL_history = dataParser("AAPL", "history")
 # MktInput = MktInput.append {"GOOGL" : int(dataParser("GOOGL", "latest"))}
 # MktInput = MktInput.append(dataParser("INTC", "history"))
 # MktInput = MktInput.append(dataParser("MFST", "history"))
@@ -13,7 +13,10 @@ accList, df= accountsCreator(True)
 
 
 cont = True
+t=1
 while (cont):
+    todaysPrices = {"AAPL": AAPL_history[-t]}
+    print("Todays price of AAPL is "+str(todaysPrices["AAPL"]))
     #get and check name
     name=input("Who will trade?")
     index=getAccount(name, accList)
@@ -33,10 +36,10 @@ while (cont):
 
 
     prevBalance = accList[index].balance
-    accList[index].balance-=MktInput[ticker]*quant
-    print(name+" bought "+str(quant)+" of "+ticker+" at "+str(MktInput[ticker])+" each. Previous balance was " + str(prevBalance)+", new balance is: "+str(accList[index].balance))
+    accList[index].balance-=todaysPrices[ticker]*quant
+    print(name+" bought "+str(quant)+" of "+ticker+" at "+str(todaysPrices[ticker])+" each. Previous balance was " + str(prevBalance)+", new balance is: "+str(accList[index].balance))
 
-
+    t+=1
     cont = input("Keep playing? Y/N").upper()
     if cont != 'Y':
         cont = False
