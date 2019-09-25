787dbe50156d768e8bbdec5cdfbbec0ada2c6cff
PvDusseldorp <pvandusseldorp@deloitte.nl>
2019-09-25 10:49:32 +0200

    adding account works

--- Classes/UI.py
+++ Classes/UI.py
@@ -1,18 +1,22 @@
 #import MarketOperator
-
-
+import csv
+import pandas as pd
 from Classes.Market import Market
 from Classes.Account import Account
 
 
 
-for i in range (2):
+for i in range (1):
     answer_1 = input("Do you want to add an account? (Y/N)").upper()
 
     if answer_1 != "Y":
         ""
     else:
-        Market.AddAccount(name = input("Please insert name: "), id = input("Please insert ID: "), balance = input("Please insert balance: "), portfolio = Account.portfolio, orders = Account.orders)
+        fields = [input("Please insert name: "), input("Please insert ID: "), input("Please insert balance: "), input('Please insert starting portfolio: '), []]
 
+        with open('ACCOUNT_DATA.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
 
-print(Market.accounts)
+df = pd.read_csv('ACCOUNT_DATA.csv')
+print(df)
