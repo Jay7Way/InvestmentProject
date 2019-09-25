e73983b9a6fecba65d8e10f45a94a36dc3963927
Jamie Kane <jamkane@deloitte.nl>
2019-09-25 09:45:32 +0200

    Remove stock function

--- Classes/Account.py
+++ Classes/Account.py
@@ -20,17 +20,32 @@ class Account:
         self.balance = balance
         self.portfolio = portfolio
 
-    def AddStock(self, name, number):
-        self.portfolio.append({"Name:": name,
-        "Number of stock": number}
+    def AddStock(self, ticker, quantity):
+        stockName = Stock.Name(ticker)
+        self.portfolio.append({"Name:": stockName,
+        "Number of stock": quantity}
         )
+
+    def RemoveStock(self, ticker, quantity):
+        stockName = Stock.Name(ticker)
+        if stockName in self.portfolio.keys():
+            if self.portfolio[stockName] > quantity:
+                self.portfolio[stockName] = self.portfolio[stockName] - quantity
+                return 0
+            else:
+                self.portfolio.remove(stockName)
+                print("Account holder not own this many stocks, we could only sell:"
+                      , quantity - self.portfolio[stockName])
+                return quantity - self.portfolio[stockName]
+        else:
+            print("Account holder does not own any of this stock")
 
     def BuyStock(self, ticker, quantity):
         self.balance +=  self.balance + Buy(ticker, quantity, "", Stock.Price(ticker))
-        self.AddStock(Stock.Name(ticker), quantity)
+        self.AddStock(ticker, quantity)
 
     def SellStock(self, ticker, quantity):
         self.balance +=  self.balance + Sell(ticker, quantity, "", Stock.Price(ticker))
-        self.portfolio.remove(ticker, quantity)
+        self.RemoveStock(ticker, quantity)
 
 
